fc24f0cb510425a47d8760a7ba1df0538a3f880b
hongdacai <1526875642@qq.com>
2024-07-13 18:49:18 +0800

    add kad-dht

--- kad-dht/main/bootstrap.go
+++ kad-dht/main/bootstrap.go
@@ -1,1 +1,36 @@
 package main
+
+import (
+	"fmt"
+	"github.com/libp2p/go-libp2p/core/peer"
+	ma "github.com/multiformats/go-multiaddr"
+)
+
+var (
+	IPFS_PEERS = convertPeers([]string{
+		"/dnsaddr/bootstrap.libp2p.io/p2p/QmNnooDu7bfjPFoTZYxMNLWUQJyrVwtbZg5gBMjTezGAJN",
+		//"/dnsaddr/bootstrap.libp2p.io/p2p/QmQCU2EcMqAqQPR2i9bChDtGNJchTbq5TbXJJ16u19uLTa",
+		"/dnsaddr/bootstrap.libp2p.io/p2p/QmbLHAnMoJPWSCR5Zhtx6BHJX9KiKNN6tpvbUcqanj75Nb",
+		//"/dnsaddr/bootstrap.libp2p.io/p2p/QmcZf59bWwK5XFi76CZX8cbJ4BhTzzA3gU1ZjYZcYW3dwt",
+		"/ip4/104.131.131.82/tcp/4001/p2p/QmaCpDMGvV2BGHeYERUEnRQAwe3N8SzbUtfsmvsqQLuvuJ", // mars.i.ipfs.io
+	})
+	LOCAL_PEERS = convertPeers([]string{
+		//"/ip4/9.134.4.207/tcp/4001/p2p/12D3KooWHGPmHztWF38xCKdQtAHdscQ3b26vwUvkSnF2oNmkXozd",
+		// It's ok
+		"/ip4/104.131.131.82/tcp/4001/p2p/QmaCpDMGvV2BGHeYERUEnRQAwe3N8SzbUtfsmvsqQLuvuJ",
+	})
+)
+
+func convertPeers(peers []string) []peer.AddrInfo {
+	pinfos := make([]peer.AddrInfo, len(peers))
+	for i, addr := range peers {
+		maddr := ma.StringCast(addr)
+		p, err := peer.AddrInfoFromP2pAddr(maddr)
+		if err != nil {
+			panic(fmt.Sprintf("parse ipfs bootstrap peers failed: err = %v", err))
+		}
+		pinfos[i] = *p
+	}
+
+	return pinfos
+}
