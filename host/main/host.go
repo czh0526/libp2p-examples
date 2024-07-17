886ac8471227e44c85b46130ab2c11325a9ee94e
hongdacai <1526875642@qq.com>
2024-07-17 14:37:58 +0800

    make host daemon

--- host/main/host.go
+++ host/main/host.go
@@ -9,40 +9,25 @@ import (
 	"github.com/libp2p/go-libp2p/core/host"
 	"github.com/libp2p/go-libp2p/core/peer"
 	"github.com/libp2p/go-libp2p/core/routing"
-	"github.com/libp2p/go-libp2p/p2p/net/connmgr"
-	"github.com/libp2p/go-libp2p/p2p/security/noise"
-	libp2ptls "github.com/libp2p/go-libp2p/p2p/security/tls"
-	"time"
 )
 
 func main() {
 
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
 	var idht *dht.IpfsDHT
 	priv, err := utils.GeneratePrivateKey("privkey.pem")
-
-	connmgr, err := connmgr.NewConnManager(
-		100,
-		400,
-		connmgr.WithGracePeriod(time.Minute))
-	if err != nil {
-		panic(err)
-	}
 
 	h, err := libp2p.New(
 		libp2p.Identity(priv),
 		libp2p.ListenAddrStrings(
 			"/ip4/0.0.0.0/tcp/9000",
 			"/ip4/0.0.0.0/udp/9000/quic"),
-		libp2p.Security(libp2ptls.ID, libp2ptls.New),
-		libp2p.Security(noise.ID, noise.New),
-		libp2p.DefaultTransports,
-		libp2p.ConnectionManager(connmgr),
-		libp2p.NATPortMap(),
 		libp2p.Routing(func(h host.Host) (routing.PeerRouting, error) {
 			idht, err = dht.New(context.Background(), h)
 			return idht, err
 		}),
-		libp2p.EnableNATService(),
 	)
 	if err != nil {
 		panic(err)
@@ -57,7 +42,9 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("libp2p peer.ID = %v, peerAddrs = %v, node address: %v",
+	fmt.Printf("libp2p peer.ID = %v, peerAddrs = %v, node address: %v \n",
 		h.ID(), h.Addrs(), addrs)
+
+	<-ctx.Done()
 
 }
