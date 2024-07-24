527f029c74bd08be7d80d051cc3de302b8132b7d
hongdacai <1526875642@qq.com>
2024-07-24 11:05:50 +0800

    combine relay with routed.

--- relay/relay/main.go
+++ relay/relay/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"github.com/czh0526/libp2p-examples/utils"
 	"github.com/libp2p/go-libp2p"
+	"github.com/libp2p/go-libp2p/core/host"
 	"github.com/libp2p/go-libp2p/p2p/protocol/circuitv2/relay"
 	"log"
 )
@@ -13,6 +14,27 @@ func main() {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
+	rhost, err := makeHost()
+	if err != nil {
+		log.Printf("Failed to create host: %v", err)
+	}
+
+	_, err = relay.New(rhost)
+	if err != nil {
+		log.Printf("Failed to instantiate the relay1: %v", err)
+		return
+	}
+
+	fmt.Printf("peer.ID = %v\n", rhost.ID())
+	fmt.Println("peer addresses: ")
+	for _, addr := range rhost.Addrs() {
+		fmt.Printf("\t=> %v\n", addr)
+	}
+
+	<-ctx.Done()
+}
+
+func makeHost() (host.Host, error) {
 	priv, err := utils.GeneratePrivateKey("privkey.pem")
 	if err != nil {
 		panic(fmt.Sprintf("get private key failed: err = %v", err))
@@ -21,24 +43,12 @@ func main() {
 	host, err := libp2p.New(
 		libp2p.Identity(priv),
 		libp2p.ListenAddrStrings(
-			"/ip4/0.0.0.0/tcp/8080"),
+			"/ip4/0.0.0.0/tcp/8000"),
 		libp2p.EnableRelay(),
 	)
 	if err != nil {
 		log.Printf("Failed to create relay1, err = %v", err)
 	}
 
-	_, err = relay.New(host)
-	if err != nil {
-		log.Printf("Failed to instantiate the relay1: %v", err)
-		return
-	}
-
-	fmt.Printf("peer.ID = %v\n", host.ID())
-	fmt.Println("peer addresses: ")
-	for _, addr := range host.Addrs() {
-		fmt.Printf("\t=> %v\n", addr)
-	}
-
-	<-ctx.Done()
+	return host, err
 }
